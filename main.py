19423f1611c6588cee0b56b0acb8e8c7eb9b7239
PetaGb <99599006+PetaGb@users.noreply.github.com>
2022-07-27 14:38:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,135 +6,51 @@ discord: PeťaG#4695
 """
 
 
-import requests
-import bs4
-import csv
 from sys import *
+from funcs import *
+import csv
+import os
 
 script, url, file = argv
 
-if len(argv) != 3:
-    print("Please follow these instructions: insert name of the script ('script.py'), url address "
-          "('https://volby.cz...') and name of the file ('file.csv'). \nTerminating...")
-    quit()
 
-else:
-    pass
+def main(name):
 
+    if len(argv) != 3:
+        print("Please follow these instructions: insert name of the script ('script.py'), url address ",
+              "('https://volby.cz...') and name of the file ('file.csv'). \nTerminating...")
+        quit()
 
-res = requests.get(url)
-election_soup = bs4.BeautifulSoup(res.text, 'html.parser')
-elems = election_soup.select('td[class="cislo"] a')
-name_elems = election_soup.select('td[class="overflow_name"]')
+    else:
+        obce = nazev_obce(url)
+        cislo = obecni_cislo(url)
+        volici = seznam_volicu(url)
+        obalky = pocet_obalek(url)
+        hlasy = platne_hlasy(url)
+        strany_1 = vysledky_1(url)
+        strany_2 = vysledky_2(url)
+        mena = jmena_stran(url)
 
+        data = zip(cislo, obce, volici, obalky, hlasy, strany_1[0], strany_1[1], strany_1[2], strany_1[3], strany_1[4],
+                   strany_1[5], strany_1[6], strany_1[7], strany_1[8], strany_1[9], strany_1[10], strany_1[11],
+                   strany_1[12], strany_2[0], strany_2[1], strany_2[2], strany_2[3], strany_2[4], strany_2[5],
+                   strany_2[6], strany_2[7], strany_2[8], strany_2[9], strany_2[10], strany_2[11], strany_2[12])
 
-def obecni_cislo():
+        if name in os.listdir():
 
-    list_cisel = []
-    for elem in elems:
-        cisla = elem.text
-        list_cisel.append(cisla)
+            with open(file, "a", newline="", encoding="ISO8859-2") as csv_soubor:
+                writer = csv.writer(csv_soubor)
+                for row in data:
+                    writer.writerow(row)
 
-    return list_cisel
-
-
-def nazev_obce():
-
-    list_obci = []
-    for name_elem in name_elems:
-        obce = name_elem.text
-        list_obci.append(obce)
-
-    return list_obci
-
-
-def seznam_volicu():
-
-    list_seznamu = []
-    for elem in elems:
-        url_to_open = "https://volby.cz/pls/ps2017nss/" + elem.get("href")
-        muni_res = requests.get(url_to_open)
-        muni_soup = bs4.BeautifulSoup(muni_res.text, 'html.parser')
-        seznamy = muni_soup.find('td', {'headers': 'sa2'}).text
-        list_seznamu.append(seznamy)
-
-    return list_seznamu
+        else:
+            with open(file, "w", newline="", encoding="ISO8859-2") as csv_soubor:
+                zahlavi = ["ČÍSLO", "OBEC", "POČET VOLIČŮ", "VYDANÉ OBÁLKY", "PLATNÉ HLASY"] + mena
+                writer = csv.writer(csv_soubor)
+                writer.writerow(zahlavi)
+                for row in data:
+                    writer.writerow(row)
 
 
-def pocet_obalek():
-
-    list_obalek = []
-    for elem in elems:
-        url_to_open = "https://volby.cz/pls/ps2017nss/" + elem.get("href")
-        muni_res = requests.get(url_to_open)
-        muni_soup = bs4.BeautifulSoup(muni_res.text, 'html.parser')
-        vydane_obalky = muni_soup.find('td', {'headers': 'sa3'}).text
-        list_obalek.append(vydane_obalky)
-
-    return list_obalek
-
-
-def platne_hlasy():
-
-    seznam_hlasu = []
-    for elem in elems:
-        url_to_open = "https://volby.cz/pls/ps2017nss/" + elem.get("href")
-        muni_res = requests.get(url_to_open)
-        muni_soup = bs4.BeautifulSoup(muni_res.text, 'html.parser')
-        hlas = muni_soup.find('td', {'headers': 'sa6'}).text
-        seznam_hlasu.append(hlas)
-
-    return seznam_hlasu
-
-
-def politicke_strany():
-
-    seznam_stran = []
-    for elem in elems:
-        url_to_open = "https://volby.cz/pls/ps2017nss/" + elem.get("href")
-        muni_res = requests.get(url_to_open)
-        muni_soup = bs4.BeautifulSoup(muni_res.text, 'html.parser')
-        pol_strany = muni_soup.find_all('td', {'class': 'overflow_name'})
-
-        ciste_strany = []
-        for strana in pol_strany:
-            cista_strana = strana.text
-            ciste_strany.append(cista_strana)
-
-        stringed_seznam = ', '.join(ciste_strany)
-        seznam_stran.append(stringed_seznam)
-
-    return seznam_stran
-
-
-nazev = nazev_obce()
-cislo = obecni_cislo()
-volici = seznam_volicu()
-obalky = pocet_obalek()
-hlasy = platne_hlasy()
-strany = politicke_strany()
-
-
-def csv_maker():
-
-    with open(file, "a", newline="", encoding="ISO8859-2") as csv_soubor:
-        zahlavi = ["ČÍSLO", "OBEC", "POČET VOLIČŮ", "VYDANÉ OBÁLKY", "PLATNÉ HLASY", "KANDIDUJÍCÍ STRANY"]
-        writer = csv.DictWriter(csv_soubor, delimiter=",", fieldnames=zahlavi)
-        writer.writeheader()
-
-        for c, n, v, o, h, s in zip(cislo, nazev, volici, obalky, hlasy, strany):
-            writer.writerow(
-                {
-
-                    "ČÍSLO": c,
-                    "OBEC": n,
-                    "POČET VOLIČŮ": v,
-                    "VYDANÉ OBÁLKY": o,
-                    "PLATNÉ HLASY": h,
-                    "KANDIDUJÍCÍ STRANY": s
-
-                }
-            )
-
-
-csv_maker()
+if __name__ == "__main__":
+    main(file)
